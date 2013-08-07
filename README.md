acac634276fa23660be79f23e5c7f1752c88af6b
error.d <error.d@gmail.com>
2013-08-07 14:10:22 +0800

    init

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ python_debug
 ============
 
 cpython debug
+基于Python-2.6.1
+加入了一个int3语句，支持在语言级为python解释器下断点
+
---
+++ debug_code.py
@@ -0,0 +1,27 @@
+class C:
+    @classmethod
+    def AAA(cls):
+        print 'aaa'
+
+    def __init__(self):
+        pass
+
+    def bbb(self):
+        print 'ccc'
+        int3
+        self.AAA()
+        c.AAA()
+        C.AAA()
+        self.AAA()
+
+    def ccc(self):
+        self.AAA()
+
+def test():
+    print 'aaa'
+    c = C()
+    c.bbb()
+    print C.AAA
+    print c.bbb
+
+test()
